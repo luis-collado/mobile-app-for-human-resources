f97bd9a9d6e009bfa3b91f878cf146879d4e037a
EnriqueRodrigo <enrique.rodrigo@alumnos.uneatlantico.es>
2023-05-05 09:33:45 +0200

    Firebaseregistermal

--- src/RegisterScreen.js
+++ src/RegisterScreen.js
@@ -1,34 +1,112 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, Image, Alert} from 'react-native';
-import {TextInput, Button, HelperText, Title} from 'react-native-paper';
+import React, { useState } from "react";
+import { View, StyleSheet, Image, Alert } from "react-native";
+import { TextInput, Button, HelperText, Title } from "react-native-paper";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { getFirestore, doc, setDoc, collection, query, where, getDocs } from "firebase/firestore";
+import firebaseApp from "./firebaseConfig"; // Asume que has creado un archivo de configuración de Firebase
 
 
-const RegisterScreen = ({route,navigation}) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
 
-  const [confirmPassword, setConfirmPassword] = useState('');
+
+const RegisterScreen = ({ route, navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordsMatch, setPasswordsMatch] = useState(true);
 
   const checkPasswordsMatch = () => {
     setPasswordsMatch(password === confirmPassword);
-  }
+  };
 
-   const handleRegister = async () => {
-    // Aquí puedes implementar la lógica de registro con tu backend
-    console.log('Nombre ', name);
-    console.log('Correo electrónico:', email);
-    console.log('Contraseña:', password);
-    const result = await saveUserDataToMySQL(name, lastName, email, password);
-    if (result.success) {
-      navigation.navigate('LoginScreen');
-    } else {
-      if (result.error.includes('Failed to save user data to MySQL')) {
-        Alert.alert('Error', 'El email ya está siendo utilizado');
-      }
+  const handleRegister = async () => {
+    const auth = getAuth(firebaseApp); // Utiliza getAuth con la configuración de Firebase
+    const db = getFirestore(firebaseApp);
+
+    const usernameQuery = query(
+      collection(db, "users"),
+      where("username", "==", username)
+    );
+    const querySnapshot = await getDocs(usernameQuery);
+
+    if (!querySnapshot.empty) {
+      setUsername({ ...username, error: "Username is already in use." });
+      return;
     }
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        console.log("Account created!");
+        const user = userCredential.user;
+        updateProfile(auth.currentUser, {
+          displayName: username,
+        });
+
+        try {
+          const docRef = await setDoc(doc(db, "users", auth.currentUser.uid), {
+            name: name,
+            lastName: lastName,
+            username: username,
+            email: email,
+          });
+          console.log("User written with ID: ", auth.currentUser.uid);
+
+          // Call the API to save user data to MySQL
+          const saveUserDataToMySQL = async (nombre, apellidos, email, contraseña) => {
+            try {
+              const response = await fetch("https://saveuserdata-2b2k6woktq-nw.a.run.app/api/saveUserData", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, nombre, apellidos, contraseña }),
+              });
+          
+              if (!response.ok) {
+                throw new Error("Failed to save user data to MySQL" + JSON.stringify({ email, nombre, apellidos, contraseña }));
+              }
+          
+              console.log("User data saved to MySQL successfully");
+              return { success: true };
+            } catch (error) {
+              console.error("Error saving user data to MySQL:", error);
+              return { success: false, error: error.message };
+            }
+          };
+
+
+
+          await saveUserDataToMySQL(
+            auth.currentUser.uid,
+            name,
+            lastName,
+            email,
+            username
+          );
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
+
+        navigation.reset({
+          index: 0,
+          routes: [{ name: "Dashboard" }],
+        });
+      })
+      .catch((error) => {
+        if (error.code === "auth/email-already-in-use") {
+          console.log("That email address is already in use!");
+          Alert.alert("Error", "El email ya está siendo utilizado");
+        }
+
+        if (error.code === "auth/invalid-email") {
+          console.log("That email address is invalid!");
+        }
+
+        console.error(error);
+      });
   };
 
   const isValidEmail = (email) => {
---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+// src/firebase-config.js
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+    apiKey: "AIzaSyD-pRpOQzPaTxt6XMuS8Ae7qLbgjgYUKEc",
+    authDomain: "tim-consulting.firebaseapp.com",
+    projectId: "tim-consulting",
+    storageBucket: "tim-consulting.appspot.com",
+    messagingSenderId: "671150873240",
+    appId: "1:671150873240:web:5a912e59a4768aacc3ceed",
+    measurementId: "G-HNJR61QHGV"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
