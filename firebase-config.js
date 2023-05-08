6dccda197e10c418390c5589eae9c5b9b2d8fa5a
Luis <luis.collado.pu@gmail.com>
2023-05-08 12:11:48 +0200

    fix error

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyD-pRpOQzPaTxt6XMuS8Ae7qLbgjgYUKEc",
-  authDomain: "tim-consulting.firebaseapp.com",
-  projectId: "tim-consulting",
-  storageBucket: "tim-consulting.appspot.com",
-  messagingSenderId: "671150873240",
-  appId: "1:671150873240:web:5a912e59a4768aacc3ceed",
-  measurementId: "G-HNJR61QHGV"
-};
