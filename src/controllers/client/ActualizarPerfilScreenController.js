0d445636e4431e429c8829da3ef18a376e9b20de
EnriqueRodrigo <enrique.rodrigo@alumnos.uneatlantico.es>
2023-05-10 10:14:37 +0200

    actualizarperfilcontroller

---
+++ src/controllers/client/ActualizarPerfilScreenController.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
+
+export const useActualizarPerfilScreenController = (route, navigation) => {
+  const { email, userData: initialUserData } = route.params;
+  const [userData, setUserData] = useState(initialUserData);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setUserData(initialUserData);
+  }, [initialUserData]);
+
+  const actualizarPerfil = async () => {
+    setError(null);
+
+    const { mis_ofertas, foto_perfil, CV, ...updatedUserData } = userData;
+
+    const requestBody = {
+      email: email,
+      userData: updatedUserData,
+    };
+
+    console.log('JSON enviado:', JSON.stringify(requestBody, null, 2));
+
+    try {
+      const response = await fetch('https://updateuserdata-2b2k6woktq-nw.a.run.app/updateUserData', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody, null, 2),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.status}`);
+      }
+      Alert.alert(
+        'Perfil Modificado',
+        'El perfil ha sido modificado correctamente',
+        [
+          {
+            text: 'OK',
+            onPress: () => navigation.goBack(),
+          },
+        ]
+      );
+    } catch (error) {
+      setError(error.message);
+      console.log('Error al actualizar los datos:', error);
+    }
+  };
+
+  const handleChange = (name, value) => {
+    setUserData({ ...userData, [name]: value });
+  };
+
+  return {
+    userData,
+    setUserData,
+    error,
+    setError,
+    actualizarPerfil,
+    handleChange,
+  };
+};
--- src/screens/client/ActualizarPerfilScreen.js
+++ src/screens/client/ActualizarPerfilScreen.js
@@ -1,60 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView, TextInput, TouchableOpacity,Alert } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, TextInput, TouchableOpacity } from 'react-native';
 import { Button } from 'react-native-paper';
 
 import styles from '../../styles/client/ActualizarPerfilScreenStyles';
+import { useActualizarPerfilScreenController } from '../../controllers/client/ActualizarPerfilScreenController';
 
 const ActualizarPerfilScreen = ({ route, navigation }) => {
-  const { email, userData: initialUserData } = route.params;
-  const [userData, setUserData] = useState(initialUserData);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    setUserData(initialUserData);
-  }, [initialUserData]);
+  const { userData, setUserData, error, actualizarPerfil, handleChange } = useActualizarPerfilScreenController(route, navigation);
 
-  const actualizarPerfil = async () => {
-    setError(null);
-
-    const { mis_ofertas,foto_perfil, CV, ...updatedUserData } = userData;
-
-    const requestBody = {
-      email: email,
-      userData: updatedUserData,
-    };
-
-    console.log('JSON enviado:', JSON.stringify(requestBody, null, 2));
-
-    try {
-      const response = await fetch('https://updateuserdata-2b2k6woktq-nw.a.run.app/updateUserData', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody, null, 2),
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error en la solicitud: ${response.status}`);
-      }
-      Alert.alert(
-        'Perfil Modificado',
-        'El perfil ha sido modificado correctamente',
-        [
-          {
-            text: 'OK',
-            onPress: () => navigation.goBack({email}),
-          },
-        ]
-      );
-    } catch (error) {
-      setError(error.message);
-      console.log('Error al actualizar los datos:', error);
-    }
-  };
-
-  const handleChange = (name, value) => {
-    setUserData({ ...userData, [name]: value });
-  };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
