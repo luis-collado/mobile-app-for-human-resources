3869e5d52d56abccf3deb035c6f9682b32ee0624
Luis <luis.collado.pu@gmail.com>
2023-05-11 12:32:55 +0200

    navigation

--- src/screens/ResetPasswordScreen.js
+++ src/screens/ResetPasswordScreen.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { TextInput, Button, Title, IconButton } from 'react-native-paper';
 
-import styles from '../styles/resetPasswordStyles';
+import styles from '../styles/ResetPasswordStyles';
 import RecuperarContraseñaScreenEmailController from '../controllers/ResetPasswordController';
 
 const ResetPasswordScreen = ({ navigation }) => {
