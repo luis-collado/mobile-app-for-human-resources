faa3bdf8228236cb6ef31f4be6a80d755b718580
EnriqueRodrigo <enrique.rodrigo@alumnos.uneatlantico.es>
2023-05-12 13:50:57 +0200

    perfilupdate

--- src/screens/client/UpdateProfileScreen.js
+++ src/screens/client/UpdateProfileScreen.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 import { Button } from 'react-native-paper';
-
 import styles from '../../styles/client/UpdateProfileStyles';
 import { useUpdateProfileController } from '../../controllers/client/UpdateProfileController';
 
 const UpdateProfileScreen = ({ route, navigation }) => {
   const { userData, setUserData, error, updateProfile, handleChange } = useUpdateProfileController(route, navigation);
 
-
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>Actualizar Perfil</Text>
+      <Button
+        onPress={() => navigation.goBack()}
+        style={[styles.buttonBack, { marginTop: 10 }]}
+        color="#ffffff"
+      >
+        Volver
+      </Button>
       {Object.keys(userData)
         .filter((key) => key !== 'mis_ofertas' && key !== 'foto_perfil' && key !== 'CV')
         .map((key) => (
@@ -19,25 +24,22 @@ const UpdateProfileScreen = ({ route, navigation }) => {
             <Text style={styles.label}>{key}:</Text>
             <TextInput
               style={styles.input}
-              value={userData[key] ? userData[key].toString() : ''} // Verificación añadida
+              value={userData[key] ? userData[key].toString() : ''}
               onChangeText={(value) => handleChange(key, value)}
+              underlineColorAndroid="transparent"
             />
           </View>
         ))}
 
-      {error && <Text>Error al actualizar los datos: {error}</Text>}
+      {error && <Text style={styles.errorText}>Error al actualizar los datos: {error}</Text>}
+      
       <Button mode="contained" onPress={updateProfile} style={styles.button}>
         Guardar cambios
       </Button>
-       <Button
-          onPress={() => navigation.goBack()}
-          style={[styles.buttonback, { marginTop: 10 }]}
-        >
-          Volver
-        </Button>
+
+     
     </ScrollView>
   );
 };
-
 
 export default UpdateProfileScreen;
--- src/styles/client/UpdateProfileStyles.js
+++ src/styles/client/UpdateProfileStyles.js
@@ -1,48 +1,54 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-      flexGrow: 1,
-      justifyContent: 'flex-start',
-      backgroundColor: '#fff',
-      paddingTop: 60,
-      paddingBottom: 80,
-      paddingHorizontal: 20,
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginBottom: 20,
-      textAlign: 'left',
-    },
-    inputContainer: {
-      marginBottom: 15,
-    },
-    label: {
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
-    input: {
-      fontSize: 16,
-      borderColor: '#ccc',
-      borderWidth: 1,
-      borderRadius: 4,
-      paddingLeft: 8,
-      paddingRight: 8,
-      paddingTop: 4,
-      paddingBottom: 4,
-      marginBottom: 10,
-    },
-    button: {
-      backgroundColor: '#d5bf19',
-      alignSelf: 'flex-start',
-    },
-    buttonback: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginBottom: 20,
-      textAlign: 'left',
-    },
-  });
+  container: {
+    flexGrow: 1,
+    justifyContent: 'flex-start',
+    backgroundColor: '#fff',
+    padding: 30,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#d5bf19',
+    top: 10,
+  },
+  inputContainer: {
+    marginBottom: 25,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#666',
+    marginBottom: 10,
+  },
+  input: {
+    fontSize: 16,
+    borderColor: '#d5bf19',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+    backgroundColor: '#f9f9f9',
+  },
+  errorText: {
+    fontSize: 16,
+    color: 'red',
+    marginBottom: 15,
+  },
+  button: {
+    backgroundColor: '#d5bf19',
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+  },
+  buttonBack: {
+    backgroundColor: '#d5bf19',
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    borderRadius: 10,
+    color:"#ffffff",
+  },
+});
 
-  export default styles;
+export default styles;
